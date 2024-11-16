70cbcfb0a80f761ec3eabb9f7929e0a166a7ea85
David <p5y4@laposte.net>
2024-11-16 14:33:13 +0100

    90% responsive finished

--- src/components/CommentCreate.tsx
+++ src/components/CommentCreate.tsx
@@ -1,7 +1,6 @@
 "use client"
-import React, { useState } from 'react'
-import Button from './Button'
-import Modal from './Modal';
+import React, { useState } from 'react';
+import Button from './Button';
 
 interface IData {
     author: string;
